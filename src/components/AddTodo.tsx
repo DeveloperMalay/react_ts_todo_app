8796fd431cdb73a6ba95f33373faf829999d80b8
Malay Pandit <malay.pandit@dinereel.com>
2024-06-12 13:13:39 +0530

    making some changes

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,11 +1,22 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function AddTodo() {
   const [input, setInput] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleSubmission = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("form has been submitted");
+  };
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
   return (
     <div className="flex items-center w-full max-w-lg gap-2 p-5 m-auto">
-      <form>
+      <form onSubmit={handleSubmission}>
         <input
+          ref={inputRef}
           value={input}
           type="text"
           onChange={(e) => setInput(e.target.value)}
